afca34c88a85d858db1d47293a64e0e12993e5a7
geekybirdone <alan.lean@hotmail.com>
2025-06-16 15:12:53 -0300

    Finalização do exercício

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+        //Criação do Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Definição das variáveis
+        int numberCount;
+        String agency;
+        String clientName;
+        float balance;
+
+        //Solicição das informações
+        System.out.println("--------------------------------------------");
+        System.out.println("Por favor, digite os dados de sua conta");
+        System.out.println("--------------------------------------------");
+        System.out.print("\nDigite o número da sua conta: ");
+        numberCount = sc.nextInt();
+        System.out.print("Digite o número da sua agência (com o dígito): ");
+        agency = sc.next();
+        System.out.print("Digite o nome do titular: ");
+        clientName = sc.next();
+        System.out.print("Digite o valor do saldo inicial: ");
+        balance = sc.nextFloat();
+
+        //Impressão dos dados na tela
+        System.out.println("\n--------------------------------------------");
+        System.out.println("Dados da conta");
+        System.out.println("--------------------------------------------");
+        System.out.printf("\nOlá, %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.2f já está disponível para saque.", clientName, agency, numberCount, balance);
+
+        //Fechamento do Scanner
+        sc.close();
     }
 }
